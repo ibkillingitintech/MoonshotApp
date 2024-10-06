b7817e6b3265eec0ac95ffdee95e78304f8c6924
Ibraheem <ibraheem.r@outlook.com>
2024-10-06 07:47:56 -0400

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Ibraheem k. Rawlinson on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
